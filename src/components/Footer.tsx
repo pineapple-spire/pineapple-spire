78b382589ce4544f1d9520973ba8a19cd0f1eb0b
Kye Steele <143579299+kyesteele@users.noreply.github.com>
2025-01-28 00:10:32 -1000

    Footer was updated to align more with mockup.
    Links will need to be updated to sections as they are created. Only a Spire Hawaii link was added for now.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,47 @@
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import Image from 'next/image';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 footer">
     <Container>
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        <br />
-        <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
-      </Col>
+      <Row>
+        {/* TODO: Update this to have links to contact info, social media, disclaimer, and support sections. */}
+        <Col className="text-center g-3">
+          Contact Us
+          <br />
+          <a href="https://www.spirehawaii.com/">Visit Spire LLP</a>
+        </Col>
+        <Col className="text-center g-3">
+          <Row className="justify-content-center align-items-center">
+            <Col xs="auto">
+              <Image
+                src="/x.png"
+                alt="X"
+                width={50}
+                height={50}
+                style={{ objectFit: 'contain' }}
+              />
+            </Col>
+            <Col xs="auto">
+              <Image
+                src="/linkedin.png"
+                alt="LinkedIn"
+                width={50}
+                height={50}
+                style={{ objectFit: 'contain' }}
+              />
+            </Col>
+          </Row>
+          <br />
+          @ Spire Hawaii LLP 2025
+        </Col>
+        <Col className="text-center g-3">
+          Disclaimer
+          <br />
+          Report A Problem
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
