45a751a40f19a972fe633947e87272ac8dcab408
Jake Dickinson <92128947+jaked332@users.noreply.github.com>
2025-02-05 18:36:03 -1000

    Remove Prisma Tests

--- tests/john-pages.spec.ts
+++ tests/john-pages.spec.ts
@@ -19,11 +19,6 @@ test('Browse Page Loading', async ({ page }) => {
   expect(response?.status()).toBe(200);
 });
 
-test('Stress Test Tool Page Loading', async ({ page }) => {
-  const response = await page.goto('http://localhost:3000/stress-test-tool');
-  expect(response?.status()).toBe(200);
-});
-
 test('Financial Compilation Page Loading', async ({ page }) => {
   const response = await page.goto('http://localhost:3000/financial-compilation');
   expect(response?.status()).toBe(200);
